7df3fd88de6686e7e99a5ed00a464dba07dd4713
CarlosMarch <carlosmarchinfo@gmail.com>
2020-04-25 20:32:42 +0200

    make it better

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const port = process.env.PORT || 3000 // Heroku will need the PORT environment v
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
