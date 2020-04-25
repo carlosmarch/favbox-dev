a2525f60bbd03091d9b34a4d0379fb80b1a8799c
CarlosMarch <carlosmarchinfo@gmail.com>
2020-04-25 20:00:43 +0200

    make it better

---
+++ precache-manifest.318c5271b8423cfedb934ca7fe54a2d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5634c84144bbd7ca4cfda50a753052f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f95c2a1c079b9d1eb180",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "8ae1383be93081b64976",
+    "url": "/static/css/main.5ad6c74a.chunk.css"
+  },
+  {
+    "revision": "f95c2a1c079b9d1eb180",
+    "url": "/static/js/2.69be8528.chunk.js"
+  },
+  {
+    "revision": "fec4ce6c3a4b957fb4247b61b7babcc0",
+    "url": "/static/js/2.69be8528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae1383be93081b64976",
+    "url": "/static/js/main.4880f5e1.chunk.js"
+  },
+  {
+    "revision": "c1742d3bfedc48b34659",
+    "url": "/static/js/runtime-main.460497b0.js"
+  },
+  {
+    "revision": "389bf9995634e3cea9573e7f2060de69",
+    "url": "/static/media/Discover.389bf999.svg"
+  },
+  {
+    "revision": "143a351be5444b5864787eab62c3f9c7",
+    "url": "/static/media/Heart.143a351b.svg"
+  },
+  {
+    "revision": "e07c0beddbb2e9bfc80c0d7c2e669c3e",
+    "url": "/static/media/HeartFull.e07c0bed.svg"
+  },
+  {
+    "revision": "366ff493a0e4fa5999dc07f2fb0c949c",
+    "url": "/static/media/Plus.366ff493.svg"
+  },
+  {
+    "revision": "5c801df8d8c4be9963b400c997bbb39f",
+    "url": "/static/media/Search.5c801df8.svg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "3a38ace7bf97bb2f4d0c0c1ced314cab",
+    "url": "/static/media/User.3a38ace7.svg"
+  }
+]);
